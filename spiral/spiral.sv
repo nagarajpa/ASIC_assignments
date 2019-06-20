a76bbcfc21429b2888ccc017e05d8f4bf198fe2b
Nagaraj Putti <nagarajpa@google.com>
2019-06-19 22:45:08 -0700

    spec added.

--- spiral/spiral.sv
+++ spiral/spiral.sv
@@ -5,7 +5,26 @@
 // The matrix outputs the matrix elemenets in spiral form i.e it traces the rows and columns in 
 //spiral fashion.
 //    -> -> -> ->
-
+//  Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral order.
+//  
+//  Example 1:
+//  
+//  Input:
+//  [
+//   [ 1, 2, 3 ],
+//   [ 4, 5, 6 ],
+//   [ 7, 8, 9 ]
+//  ]
+//  Output: [1,2,3,6,9,8,7,4,5]
+//  Example 2:
+//  
+//  Input:
+//  [
+//    [1, 2, 3, 4],
+//    [5, 6, 7, 8],
+//    [9,10,11,12]
+//  ]
+//  Output: [1,2,3,4,8,12,11,10,9,5,6,7]
 ///////////////////////////////////////////////////////////
 
 module spiral #(
