421b44cce7181d0999fb66566d7aaa7c85b17a27
Nagaraj Putti <nagarajpa@google.com>
2019-07-25 07:31:04 -0700

    Indenntation

--- comparator/comparator.sv
+++ comparator/comparator.sv
@@ -13,55 +13,55 @@ module comparator
   output logic [$clog2(NUM_INPUTS)-1 :0]  out
 );
 
-generate if (BINARY_TREE == 0) begin
-
-  always_comb begin
-    out      = '0;
-    for (int i =0; i < NUM_INPUTS; ++i) begin
-      if (cmp_ip[i] > cmp_ip[out]) begin
-        out      = i;
-      end
-    end
-  end
-
-end else begin
+  generate if (BINARY_TREE == 0) begin
   
-  logic [$clog2(NUM_INPUTS)-1 : 0] out_stg[$clog2(NUM_INPUTS) + 1][NUM_INPUTS];
-  logic [DATA_WIDTH-1 : 0]         cmp_stg[$clog2(NUM_INPUTS) + 1][NUM_INPUTS];
-  
-  
-  always_comb begin
-    for (int i = 0; i < NUM_INPUTS; i++) begin
-      cmp_stg[0][i]  = cmp_ip[i];
-      out_stg[0][i]  = i;
-    end
-  end
-  
-  
-  always_comb begin
-    for (int i = 0;i < $clog2(NUM_INPUTS); i++) begin
-      for (int j = 0;j < NUM_INPUTS; ++j) begin
-        out_stg[i+1][j]    = 'X;
-        cmp_stg[i+1][j]    = 'X;
-      end
-    end
-  
-  
-    for (int i = 0;i < $clog2(NUM_INPUTS); i++) begin
-      for (int j = 0;j < (NUM_INPUTS/2**i); j= j+2) begin
-        if (cmp_stg[i][j] > cmp_stg[i][j+1]) begin
-          out_stg[i+1][j/2]    = out_stg[i][j];
-          cmp_stg[i+1][j/2]    = cmp_stg[i][j];
-        end else begin
-          out_stg[i+1][j/2]    = out_stg[i][j+1];
-          cmp_stg[i+1][j/2]    = cmp_stg[i][j+1];
+    always_comb begin
+      out      = '0;
+      for (int i =0; i < NUM_INPUTS; ++i) begin
+        if (cmp_ip[i] > cmp_ip[out]) begin
+          out      = i;
         end
       end
     end
-    out   = out_stg[$clog2(NUM_INPUTS)][0];
+  
+  end else begin
+    
+    logic [$clog2(NUM_INPUTS)-1 : 0] out_stg[$clog2(NUM_INPUTS) + 1][NUM_INPUTS];
+    logic [DATA_WIDTH-1 : 0]         cmp_stg[$clog2(NUM_INPUTS) + 1][NUM_INPUTS];
+    
+    
+    always_comb begin
+      for (int i = 0; i < NUM_INPUTS; i++) begin
+        cmp_stg[0][i]  = cmp_ip[i];
+        out_stg[0][i]  = i;
+      end
+    end
+    
+    
+    always_comb begin
+      for (int i = 0;i < $clog2(NUM_INPUTS); i++) begin
+        for (int j = 0;j < NUM_INPUTS; ++j) begin
+          out_stg[i+1][j]    = 'X;
+          cmp_stg[i+1][j]    = 'X;
+        end
+      end
+    
+    
+      for (int i = 0;i < $clog2(NUM_INPUTS); i++) begin
+        for (int j = 0;j < (NUM_INPUTS/2**i); j= j+2) begin
+          if (cmp_stg[i][j] > cmp_stg[i][j+1]) begin
+            out_stg[i+1][j/2]    = out_stg[i][j];
+            cmp_stg[i+1][j/2]    = cmp_stg[i][j];
+          end else begin
+            out_stg[i+1][j/2]    = out_stg[i][j+1];
+            cmp_stg[i+1][j/2]    = cmp_stg[i][j+1];
+          end
+        end
+      end
+      out   = out_stg[$clog2(NUM_INPUTS)][0];
+    end
   end
-end
-endgenerate
+  endgenerate
 
 
 
